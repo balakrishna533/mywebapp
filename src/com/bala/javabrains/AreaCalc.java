387cbe24373dd46167ebd64548b01e902f46af9b
Balakrishna Kancharana <balakrishna533@gmail.com>
2018-05-30 14:55:37 +0800

    This is my 3rd commit

--- src/com/bala/javabrains/AreaCalc.java
+++ src/com/bala/javabrains/AreaCalc.java
@@ -3,7 +3,7 @@ package com.bala.javabrains;
 public class AreaCalc {
 
 	public static void main(String[] args) {
-		System.out.println("Till now now area calculated is modified.");
+		System.out.println("Till now now area calculated is modified second time");
 	}
 
 }
