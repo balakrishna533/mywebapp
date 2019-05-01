2232783b1f5e6ab770c21f35b84ba20571e9876a
Balakrishna Kancharana <balakrishna533@gmail.com>
2019-05-01 15:14:14 +0800

    Venkat changes

--- src/com/bala/javabrains/AreaCalc.java
+++ src/com/bala/javabrains/AreaCalc.java
@@ -4,6 +4,7 @@ public class AreaCalc {
 
 	public static void main(String[] args) {
 		System.out.println("Till now now area calculated is modified second time");
+		System.out.println("Statement added by Venka");
 	}
 
 }
